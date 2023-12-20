f789c2949a1ee0f1700b5b6a0419c8aa5ab5cdf8
sdenheyer <stevedenheyer@gmail.com>
2023-12-20 15:25:29 -0500

    Format entity added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
